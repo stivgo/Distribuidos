090d04c22dd2cbd68e338a8aae08516be2abb83f
stivgo <stiveng0630@gmail.com>
2021-09-30 01:18:41 -0500

    Implements DHT

--- DHT/index.js
+++ DHT/index.js
@@ -1,23 +1,73 @@
-const zmq = require("zeromq");
+('use strict');
+var kad = require('kademlia-dht');
+const zmq = require('zeromq');
 
-const sock = new zmq.Reply
+const sock = new zmq.Reply();
+let DHT1;
+let DHT2;
 
-const main =  async () => {
+const main = async () => {
+  
   console.log('Servidor DHT escuchando puerto 8002');
+  
   try {
-    await sock.bind("tcp://127.0.0.1:8002");
+    await sock.bind('tcp://127.0.0.1:8002');
     for await (const [msg] of sock) {
-      const data = JSON.parse(msg.toString())
+      const data = JSON.parse(msg.toString());
       let b = Buffer.from(data.data);
       let s = b.toString('utf-8');
       let o = JSON.parse(s);
-      console.log(o) 
-      const sendInfo = Buffer.from(JSON.stringify("Buenas llega a DHT"))
-      await sock.send(sendInfo)
+      o.map(element=> setInfo(element.idEmpleador+element.idOferta, 
+        element.Clasificacion+" "+element.Nombre))
+      getInfo('110')
+      const sendInfo = Buffer.from(JSON.stringify('recepcion DHT'));
+      await sock.send(sendInfo);
     }
   } catch (err) {
-    sock.send("No inserta la infomación")
+    sock.send('No inserta la infomación');
     console.log(err);
   }
 };
+createSpawnNode();
 main();
+
+
+// Store a value on one side and get it back on the other side.
+//
+function setInfo(key, value) {
+  DHT1.set(key, value, function (err) {
+    
+  });
+}
+
+function getInfo(key) {
+  DHT2.get(key, function (err, value) {
+    console.log("valor de la key : "+ key+ " es : " + value);
+  });
+  console.log("---DHT1 HASH---_")
+  console.log(DHT1._locals)
+}
+
+
+
+// Spawn a node. A node is composed of two elements: the local Dht and the Rpc.
+//
+function spawnNode(endpoint, seeds, cb) {
+  kad.MockRpc.spawn(endpoint, function (err, rpc) {
+    if (err) return cb(err);
+    kad.Dht.spawn(rpc, seeds, function (err, dht) {
+      if (err) return cb(err);
+      cb(err, dht);
+    });
+  });
+}
+
+function createSpawnNode() {
+  spawnNode('localhost:9876', [], function (err, dht1) {
+    spawnNode('localhost:4321', [dht1.rpc.endpoint], function (err, dht2) {
+      DHT1 = dht1;
+      DHT2 = dht2;
+    });
+  });
+}
+
