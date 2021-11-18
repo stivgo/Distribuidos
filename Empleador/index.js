98245eeed82c6cdef14b7ee2c0af3ae456044ed2
stivgo <stiveng0630@gmail.com>
2021-11-17 22:28:39 -0500

    Update Empleador

--- Empleador/index.js
+++ Empleador/index.js
@@ -3,7 +3,8 @@ const Oferta = require('../Common/Oferta.js');
 let sectores = [];
 
 const zmq = require('zeromq');
-const sockFiltro = new zmq.Publisher();
+const sockPubFiltro = new zmq.Publisher();
+const sockSubFiltro = new zmq.Subscriber();
 
 const servidor = express();
 servidor.use(express.json());
@@ -21,7 +22,7 @@ servidor.post('/empleador', async (req, res, next) => {
   console.log(req.body);
   try {
     const buf = Buffer.from(JSON.stringify(req.body));
-    await sockFiltro.send(['Ofertas', buf]);
+    await sockPubFiltro.send(['Ofertas', buf]);
     res.status(201).json({ data: 'Se envio el empleo' });
   } catch (error) {
     console.log(error);
@@ -43,8 +44,28 @@ servidor.post('/empleador/sub', async (req, res, next) => {
   }
 });
 
+async function sockSubFiltroOn() {
+  for await (const [topic, msg] of sockSubFiltro) {
+    console.log('Topic: ',String(topic),'\n','Message: ',JSON.parse(msg));
+    let oferta = Oferta.fromJSON(msg);
+    try {
+      console.log("Se actualizo la oferta publicada :\n"+oferta.toJSON());
+    } catch (error) {
+      console.log(error);
+    }
+    console.log("termine")
+  }
+}
+
 servidor.listen(3000, async () => {
-  await sockFiltro.bind('tcp://127.0.0.1:8001');
+  await sockPubFiltro.bind('tcp://127.0.0.1:8001');
   console.log('Publisher bound to sport 8001');
+  
+  sockSubFiltro.connect('tcp://127.0.0.1:8002');
+  sockSubFiltro.subscribe('Respuesta');
+  sockSubFiltroOn();
+  console.log('Subscriber Filtro connected to port 8002');
+  
+
   console.log('Servidor escuchando puerto 3000');
 });
