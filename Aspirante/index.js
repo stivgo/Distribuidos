ed8052e2109599020267522fec22247d109aa296
stivgo <stiveng0630@gmail.com>
2021-11-17 22:26:49 -0500

    Actualización Aspirante

--- Aspirante/index.js
+++ Aspirante/index.js
@@ -3,7 +3,8 @@ const Oferta = require('../Common/Oferta.js');
 let sectores = [];
 
 const zmq = require('zeromq');
-const sock = new zmq.Publisher();
+const sockPubFiltro = new zmq.Publisher();
+const sockSubFiltro = new zmq.Subscriber();
 
 const servidor = express();
 servidor.use(express.json());
@@ -21,7 +22,7 @@ servidor.post('/aspirante', async (req, res, next) => {
   console.log(req.body);
   try {
     const buf = Buffer.from(JSON.stringify(req.body));
-    await sock.send(['Ofertas', buf]);
+    await sockPubFiltro.send(['Ofertas', buf]);
     res.status(201).json({ data: 'Se envio hoja de vida' });
   } catch (error) {
     console.log(error);
@@ -43,8 +44,27 @@ servidor.post('/aspirante/sub', async (req, res, next) => {
   }
 });
 
+async function sockSubFiltroOn() {
+  for await (const [topic, msg] of sockSubFiltro) {
+    console.log('Topic: ',String(topic),'\n','Message: ',JSON.parse(msg));
+    let oferta = Oferta.fromJSON(msg);
+    try {
+      console.log("Tiene una nueva oferta :\n"+oferta.toJSON());
+    } catch (error) {
+      console.log(error);
+    }
+    console.log("termine")
+  }
+}
+
 servidor.listen(3002, async () => {
-  await sock.bind('tcp://127.0.0.1:8003');
+  await sockPubFiltro.bind('tcp://127.0.0.1:8003');
   console.log('Publisher bound to sport 8003');
+  
+  sockSubEmpleador.connect('tcp://127.0.0.1:8004');
+  sockSubEmpleador.subscribe('Respuesta');
+  console.log('Subscriber Empleador connected to port 8004');
+  sockSubFiltroOn();
+  
   console.log('Servidor escuchando puerto 3002');
 });
